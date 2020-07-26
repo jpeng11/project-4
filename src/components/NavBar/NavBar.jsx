ea370909947a248344a0eb243745d90e7bed9015
Percy <percy.pjc@gmail.com>
2020-07-26 01:16:20 -0400

    Add stying for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,31 +2,72 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
-import { Button } from "semantic-ui-react";
+import { Button, Menu, Segment } from "semantic-ui-react";
 
-const NavBar = (props) => {
-  let nav = props.user ? (
-    <div className="NavBar">
-      <Button as={Link} to="/login">
-        Login
-      </Button>
-      &nbsp;&nbsp;
-      <Button as={Link} to="" onClick={props.handleLogout}>
-        LOG OUT
-      </Button>
-    </div>
-  ) : (
-    <div className="NavBar">
-      <Button as={Link} to="/login">
-        Login
-      </Button>
-      &nbsp;&nbsp;
-      <Button as={Link} to="/signup">
-        Sign up
-      </Button>
-    </div>
-  );
-  return <div className="NavBar">{nav}</div>;
-};
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: "home" };
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return this.props.user ? (
+      <Segment>
+        <Menu pointing secondary>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="messages"
+            active={activeItem === "messages"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="friends"
+            active={activeItem === "friends"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item name="Log in" as={Link} to="/login" position="right" />
+          <Menu.Item
+            name="Log out"
+            onClick={this.props.handleLogout}
+            as={Link}
+            to=""
+          />
+        </Menu>
+      </Segment>
+    ) : (
+      <Segment>
+        <Menu pointing secondary>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/"
+          />
+          <Menu.Item
+            name="messages"
+            active={activeItem === "messages"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="friends"
+            active={activeItem === "friends"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item name="Log in" as={Link} to="/login" position="right" />
+          <Menu.Item name="Sign up" as={Link} to="/signup" />
+        </Menu>
+      </Segment>
+    );
+  }
+}
 
 export default NavBar;
